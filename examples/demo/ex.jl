d8ef2ab3f8e570e862f7e85ffa7b404c96b40ea3
Daniel O'Malley <omalled@lanl.gov>
2017-01-26 12:00:32 -0700

    basic implementation seems to be working

--- examples/demo/ex.jl
+++ examples/demo/ex.jl
@@ -1,3 +1,4 @@
+using Base.Test
 import Origami
 
 srand(1)
@@ -5,3 +6,10 @@ B = rand(2, 3)
 C = rand([0, 1], 3, 2)
 A = B * C
 Cest = Origami.solvequbo(A, B)
+@test_approx_eq_eps A B * Cest 1e-6
+Best = Origami.solvelsq(A, C)
+@test_approx_eq_eps A Best * C 1e-6
+
+Best, Cest = Origami.factor(A, 3; tol=1e-6)
+#vecnorm(A - Best2 * Cest2)
+@test_approx_eq_eps A Best * Cest 1e-6
--- src/Origami.jl
+++ src/Origami.jl
@@ -1,6 +1,8 @@
 module Origami
 
-using ThreeQ
+import ThreeQ
+import JuMP
+import Ipopt
 
 #the basic setup here is A = B*C
 #where A is a real-valued matrix of observations, B is an unknown real-valued matrix, and C is an unknown binary matrix
@@ -9,18 +11,48 @@ using ThreeQ
 ```
 solvequbo(A, B)
 ```
-Solves returns a binary matrix C that makes ||A - B*C|| small.
+Returns a binary matrix C that makes ||A - B*C|| small.
 """
 function solvequbo(A, B)
-	m = ThreeQ.Model("Origami", "laptop", "c4-sw_sample", "workingdir", "c4")
-	@defvar m C[1:size(B, 2), 1:size(A, 2)]
-	#TODO you can decompose the problem into one QUBO for each column of C
-	for i = 1:size(A, 1), j = 1:size(A, 2)
-		#A[i, j] == B[i, :]⋅C[:, j]
-		ThreeQ.addlinearconstraint!(m, A[i, j], B[i, :], [C[k, j] for k in 1:size(C, 1)])
+	C = Array(Int, size(B, 2), size(A, 2))
+	#decompose the problem into one QUBO for each column of C
+	for j = 1:size(A, 2)
+		m = ThreeQ.Model("Origami", "laptop", "c4-sw_sample", "workingdir", "c4")
+		@ThreeQ.defvar m Ccolj[1:size(B, 2)]
+		for i = 1:size(A, 1)
+			#A[i, j] == B[i, :]⋅C[:, j]
+			ThreeQ.addlinearconstraint!(m, A[i, j], B[i, :], [Ccolj[k] for k in 1:size(B, 2)])
+		end
+		ThreeQ.qbsolv!(m; showoutput=false)
+		C[:, j] = Ccolj.value
 	end
-	ThreeQ.qbsolv!(m; showoutput=false)
-	return C.value
+	return C
+end
+
+"""
+```
+solvelsq(A, C)
+```
+Returns a real-valued matrix B that makes ||A - B*C|| small.
+"""
+function solvelsq(A, C; max_iter=100, print_level=0)
+	m = JuMP.Model(solver=Ipopt.IpoptSolver(max_iter=max_iter, print_level=print_level))
+	@JuMP.variable(m, B[i=1:size(A, 1), j=1:size(C, 1)])
+	@JuMP.constraint(m, B .>= 0)
+	@JuMP.objective(m, Min, sum((A[i, j] - sum(B[i, k] * C[k, j] for k = 1:size(C, 1))) ^ 2 for i = 1:size(A, 1), j = 1:size(A, 2)))
+	JuMP.solve(m)
+	return JuMP.getvalue(B)
+end
+
+function factor(A, k; B=rand(size(A, 1), k), C=rand([0, 1], k, size(A, 2)), max_iter=100, max_lsq_iter=100, print_level=0, tol=1e-6)
+	for i = 1:max_iter
+		C = solvequbo(A, B)
+		B = solvelsq(A, C; max_iter=max_lsq_iter, print_level=print_level)
+		if vecnorm(A - B * C) < tol
+			break
+		end
+	end
+	return B, C
 end
 
 end
